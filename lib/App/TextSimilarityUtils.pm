b6278c41235ee5cd73a2039f524cfbe0ef478f46
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2024-01-18 15:33:45 +0700

    Initial commit

---
+++ lib/App/TextSimilarityUtils.pm
@@ -0,0 +1,29 @@
+package App::TextSimilarityUtils;
+
+use 5.010001;
+use strict;
+use warnings;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+our %SPEC;
+
+1;
+#ABSTRACT: CLI utilities related to text similarity
+
+=head1 DESCRIPTION
+
+This distributions provides the following command-line utilities related to
+text similarity:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<App::LevenshteinModules>
+
+L<grep-similar-text> from L<App::grep::similar::text>
